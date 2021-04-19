e4c7cfa7195e7bf712b9bc43f6ef774d00aa6c5d
Ricarth Lima <rrsl@cin.ufpe.br>
2021-04-19 14:31:12 -0300

    sign up route

--- lib/shared/constants/routes.dart
+++ lib/shared/constants/routes.dart
@@ -1,7 +1,4 @@
 class Routes {
   static const signUp =
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-9dBxrULBerSR671lVDrm1gsW8Ze8-wQ";
-
-  static const login =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC-9dBxrULBerSR671lVDrm1gsW8Ze8-wQ";
 }
