b5958bc51b5c4a36b4d52ad45e9eb8b099093750
ricarthlima <rrsl@cin.ufpe.br>
2021-07-22 17:12:20 -0300

    fixed routes

--- lib/pages/sign_up/sign_up_service.dart
+++ lib/pages/sign_up/sign_up_service.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 class SignUpService {
   signUp(String email, String password) async {
     http.Response response = await http.post(
-      Routes.signUp,
+      Uri.parse(Routes().signIn()),
       body: json.encode(
         {
           "email": email,
--- lib/shared/constants/routes.dart
+++ lib/shared/constants/routes.dart
@@ -1,4 +1,14 @@
 class Routes {
-  static const signUp =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-9dBxrULBerSR671lVDrm1gsW8Ze8-wQ";
+  static const String authService =
+      "https://identitytoolkit.googleapis.com/v1/";
+
+  static const String apiKey = "AIzaSyC-9dBxrULBerSR671lVDrm1gsW8Ze8-wQ";
+
+  String signIn() {
+    return authService + "accounts:signInWithPassword?key=" + apiKey;
+  }
+
+  String signUp() {
+    return authService + "accounts:signUp?key=" + apiKey;
+  }
 }
